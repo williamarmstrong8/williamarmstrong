58f22fd340b79e92082cbf28b2d27899296c468c
William Armstrong <williamarmstrong8@gmail.com>
2025-12-03 16:22:55 -0500

    update

--- src/components/FeaturedProjectsCard.tsx
+++ src/components/FeaturedProjectsCard.tsx
@@ -9,11 +9,6 @@ const FeaturedProjectsCard = () => {
   
   const allProjects = [
     {
-      title: "Sort It Out - Client Web App",
-      category: "Engineering",
-      image: "/projects/sort_it_out/home.png"
-    },
-    {
       title: "AI Blog Generator",
       category: "Engineering",
       image: "/projects/AI Blog Generator/articles.png"
